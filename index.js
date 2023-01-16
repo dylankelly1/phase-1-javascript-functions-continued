23531002189607995b89df8a1e3ffb01710a5975
Dylan Kelly <init.defaultBranch>
2023-01-16 16:35:47 -0600

    commit tree f448a57376fb78b26df74db5a31f942356a0bf58

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(activity = 'go to the office') {
+    return(`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(param="*") {
+    const part1 = "You are"
+    return function innerFunction(param2="special") {
+return `${part1} ${param}${param2}${param}!`
+    };
+}
